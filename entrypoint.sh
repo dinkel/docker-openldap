b165002180fa65e21a1b9c0686daee97a9fffd43
Christian Luginbühl <dinkel@pimprecords.com>
2015-03-25 16:46:27 +0100

    Added sane open file descriptors limit based on request by Hong Xu (xuhdev)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# When not limiting the open file descritors limit, the memory consumption of
+# slapd is absurdly high. See https://github.com/docker/docker/issues/8231
+ulimit -n 8192
+
 chown -R openldap:openldap /var/lib/ldap/
 
 if [[ ! -d /etc/ldap/slapd.d ]]; then
